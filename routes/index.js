4859ddaf11b0e71b36911cdf6035a510bf127ff7
haard <a1797163@student.adelaide.edu.au>
2021-06-04 03:53:44 +0000

    trying login stuff

--- routes/index.js
+++ routes/index.js
@@ -7,62 +7,61 @@ router.get('/', function(req, res, next) {
 });
 
 
+// router.get('/rfrfrf', function(req,res,next){
+//     if (req.query.vname.length > 0){
+//         var vname = req.query.vname;
+//     }
+//     if (req.query.vaddress.length > 0){
+//         var vaddress = req.query.vaddress;
+//     }
+//     //if date...
+//     //if time...
+//     //if time...
+//     /* var query = "SELECT venueName, venueAddress, phoneNumber, checkInDate, checkInTime
+//     FROM checkIns
+//     WHERE (venueName LIKE '%?%' OR venueAddress LIKE "%?%" OR startTime LIKE ... OR endTime LIKE ...)
+//     */
+//     //connection.query...
+// });
 
-router.get('/rfrfrf', function(req,res,next){
-    if (req.query.vname.length > 0){
-        var vname = req.query.vname;
-    }
-    if (req.query.vaddress.length > 0){
-        var vaddress = req.query.vaddress;
-    }
-    //if date...
-    //if time...
-    //if time...
-    /* var query = "SELECT venueName, venueAddress, phoneNumber, checkInDate, checkInTime
-    FROM checkIns
-    WHERE (venueName LIKE '%?%' OR venueAddress LIKE "%?%" OR startTime LIKE ... OR endTime LIKE ...)
-    */
-    //connection.query...
-});
+// router.get('/ffffff', function(req,res,next){
+//     if (req.query.fname.length > 0){
+//         var fname = req.query.fname;
+//     }
+//     if (req.query.sname.length > 0){
+//         var surname = req.query.sname;
+//     }
+//     //if date...
+//     //if time...
+//     //if time...
+//     /* var query = "SELECT givenName, surname, mobileNumber, checkInDate, checkInTime
+//     FROM checkIns
+//     WHERE (givenName LIKE '%?%' OR surname LIKE "%?%" OR date LIKE ... OR startTime LIKE ... OR endTime LIKE ...)
+//     */
+//     //connection.query...
+// });
 
-router.get('/ffffff', function(req,res,next){
-    if (req.query.fname.length > 0){
-        var fname = req.query.fname;
-    }
-    if (req.query.sname.length > 0){
-        var surname = req.query.sname;
-    }
-    //if date...
-    //if time...
-    //if time...
-    /* var query = "SELECT givenName, surname, mobileNumber, checkInDate, checkInTime
-    FROM checkIns
-    WHERE (givenName LIKE '%?%' OR surname LIKE "%?%" OR date LIKE ... OR startTime LIKE ... OR endTime LIKE ...)
-    */
-    //connection.query...
-});
-
-router.get('/tttttt', function(req,res,next){
-    if (req.query.fname.length > 0){
-        var fname = req.query.fname;
-    }
-    if (req.query.sname.length > 0){
-        var surname = req.query.sname;
-    }
-    if (req.query.vname.length > 0){
-        var venueName = req.query.vname;
-    }
-    if (req.query.vaddress.length > 0){
-        var venueAddress = req.query.vaddress;
-    }
-    //if date...
-    //if time...
-    //if time...
-    /* var query = "SELECT givenName, surname, venueName, venueAddress, mobileNumber, phoneNumber, checkInDate, checkInTime
-    FROM checkIns
-    WHERE (givenName LIKE '%?%' OR surname LIKE "%?%" OR venueName LIKE "%?%" OR venueAddress LIKE "%?%" date LIKE ... OR startTime LIKE ... OR endTime LIKE ...)
-    */
-    //connection.query...
-});
+// router.get('/tttttt', function(req,res,next){
+//     if (req.query.fname.length > 0){
+//         var fname = req.query.fname;
+//     }
+//     if (req.query.sname.length > 0){
+//         var surname = req.query.sname;
+//     }
+//     if (req.query.vname.length > 0){
+//         var venueName = req.query.vname;
+//     }
+//     if (req.query.vaddress.length > 0){
+//         var venueAddress = req.query.vaddress;
+//     }
+//     //if date...
+//     //if time...
+//     //if time...
+//     /* var query = "SELECT givenName, surname, venueName, venueAddress, mobileNumber, phoneNumber, checkInDate, checkInTime
+//     FROM checkIns
+//     WHERE (givenName LIKE '%?%' OR surname LIKE "%?%" OR venueName LIKE "%?%" OR venueAddress LIKE "%?%" date LIKE ... OR startTime LIKE ... OR endTime LIKE ...)
+//     */
+//     //connection.query...
+// });
 
 module.exports = router;
